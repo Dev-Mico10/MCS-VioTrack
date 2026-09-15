23569d1f7ac2df8cbf64a26ada29473ed3318a47
My PC <micoxd81@gmail.com>
2026-09-15 16:14:46 +0800

    Added Talaoc profile test

---
+++ app/src/test/java/com/example/mcs_viotrack/Talaocminipeta.java
@@ -0,0 +1,23 @@
+package com.example.mcs_viotrack;
+
+import org.junit.Test;
+
+public class Talaocminipeta {
+    @Test
+    public void printMyprofile() {
+
+        // --- 1. THE INPUT (Storing your personal details in variables) ---
+        String myname = "mico";
+        String petName = "kyna and snowy";
+        String favFood = "Adobo";
+        int myage = 17;
+
+        // --- 2. THE OUTPUT (Printing to the console) ---
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myname + " and I am " + myage + " years old.");
+        System.out.println("I have wonderful pets named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
+}
+
+
